6038b269486cc3d3555076dce70f874a8c0c406c
Leon Kettern <leon-maurice-kettern@t-online.de>
2024-11-15 23:38:21 +0100

    added shadcn, tailwind css and fontawesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
@@ -18,6 +19,19 @@ function App() {
         >
           Learn React
         </a>
+        <a
+          className="text-blue-400 underline mt-4"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="mt-4">
+          <Button variant="secondary" className="ml-2">
+            Secondary Button
+          </Button>
+        </div>
       </header>
     </div>
   );
